5e8b0995b746995163420f5f1a132c909da794d6
bibi <me@pannnda.com>
2019-03-19 23:17:35 +0800

    完成登陆界面

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import {
+  Input,
+  Button,
+  Icon,
+  message
+} from 'antd'
+
+export const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div style={{
+      width: '360px',
+      height: '260px',
+      background: '#fff',
+      borderRadius: 10,
+      boxShadow: 'rgba(26, 26, 26, 0.1) 0px 0px 5px 0px',
+      display: 'flex',
+      alignItems: 'center',
+      flexFlow: 'column'
+    }}
+    >
+      <span style={{ marginTop: 30, fontSize: 18 }}>比赛报名后台管理系统</span>
+      <Input
+        style={{ width: '64%', marginTop: 20 }}
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        placeholder="用户名"
+        onChange={e => {
+          setUsername(e.target.value)
+        }}
+      />
+      <Input
+        style={{ width: '64%', marginTop: 15 }}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+        type="password"
+        placeholder="密码"
+        onChange={e => {
+          setPassword(e.target.value)
+        }}
+      />
+      <Button
+        style={{ width: '64%', marginTop: 25 }}
+        type="primary"
+        onClick={() => {
+          if (username === '' || password === '') return message.error('用户名或密码不能为空！')
+          console.log({
+            username,
+            password
+          })
+        }}
+      >登陆</Button>
+    </div>
+  )
+}
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { LoginForm } from '../../components/LoginForm'
 
 export const Login = () => {
   return (
-    <div>login</div>
+    <div style={{
+      position: 'fixed',
+      display: 'flex',
+      width: '100%',
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#EBEBEB'
+      }}
+    >
+      <LoginForm />
+    </div>
   )
 }
